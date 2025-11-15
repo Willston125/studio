22bf5552b618f5f4c9db4199bbe801d5994ad572
Ali William <A.Willston@gmail.com>
2025-11-15 09:33:14 +0000

    diminue l'espacement encore  (_for element h1_)

--- src/components/cine-enroll/hero-section.tsx
+++ src/components/cine-enroll/hero-section.tsx
@@ -24,7 +24,7 @@ export default function HeroSection() {
 
             <h1 
               tabIndex={0}
-              className="text-8xl font-headline font-black tracking-wide text-white leading-tight" 
+              className="text-8xl font-headline font-black tracking-wide text-white leading-none" 
               style={{ textShadow: '0 4px 15px rgba(0,0,0,0.5)' }}
             >
               Devenez Réalisateur
