af5ff432dbdfa660e9d3da17334305ce8dc639a5
Ali William <A.Willston@gmail.com>
2025-11-15 10:32:59 +0000

    trouver la solution pour apporter ma demande car je ne vois toujours pas

--- src/components/cine-enroll/hero-section.tsx
+++ src/components/cine-enroll/hero-section.tsx
@@ -23,7 +23,7 @@ export default function HeroSection() {
             
             <h1 
               tabIndex={0}
-              className="text-8xl font-headline font-black tracking-wide text-white leading-none" 
+              className="text-8xl font-headline font-black tracking-wide text-white leading-tight" 
               style={{ textShadow: '0 4px 15px rgba(0,0,0,0.5)' }}
             >
               Devenez Réalisateur
