5621f815a03ea1c1d31cd1207600807a955f1fd9
Willston125git config --global user.email <a.willston@gmail.com>
2025-11-30 20:53:55 +0300

    mise a jour complete

--- src/components/cine-enroll/hero-section.tsx
+++ src/components/cine-enroll/hero-section.tsx
@@ -142,7 +142,7 @@ export default function HeroSection() {
           </div>
 
           {/* Colonne de Droite : Vide pour l'équilibre */}
-          <div className="relative h-full w-full hidden md:flex flex-col lg:mt-32">
+          <div className="relative h-full w-full hidden md:flex flex-col lg:mt-20">
             {/* Program Section Overlay */}
             <motion.div
               initial={{ opacity: 0, x: 50 }}
