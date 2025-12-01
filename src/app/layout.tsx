2fba637abf534614fe29cf15313d0bfd7edcaea4
Willston125git config --global user.email <a.willston@gmail.com>
2025-12-01 18:50:36 +0300

    mise a jour complete

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,7 +49,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:ital,wght@0,400;0,700;1,400&family=Playfair+Display:wght@700&family=Garamond:wght@400&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:ital,wght@0,400;0,700;1,400&family=Oswald:wght@400;700&family=Playfair+Display:wght@700&family=Garamond:wght@400&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased bg-background">
         <Navbar />
