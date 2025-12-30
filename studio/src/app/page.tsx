d0808f33db6f3f8b021c0ae1a772f8dc487f0e2f
Willston125git config --global user.email <a.willston@gmail.com>
2025-12-30 21:29:46 +0300

    mise a jour

--- studio/src/app/page.tsx
+++ studio/src/app/page.tsx
@@ -4,6 +4,7 @@ import HeroSection from '@/components/cine-enroll/hero-section';
 import PhotoGallery from '@/components/cine-enroll/photo-gallery';
 import TestimonialsSection from '@/components/cine-enroll/testimonials-section';
 import ProgramPackagesSection from '@/components/cine-enroll/program-packages-section';
+import CurriculumSection from '@/components/cine-enroll/curriculum-section';
 
 import WhyChooseUsSection from '@/components/cine-enroll/why-choose-us-section';
 import { Divider } from '@/components/ui/divider';
@@ -21,9 +22,9 @@ export default function Home() {
 
       <Divider variant="gold" />
 
-      <Divider />
+      <CurriculumSection />
 
-      <Divider />
+      <Divider variant="gold" />
 
       <main>
         <WhyChooseUsSection />
