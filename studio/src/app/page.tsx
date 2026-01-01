4ca78056fbb8154560f5463c5387bb24275f1d51
Willston125git config --global user.email <a.willston@gmail.com>
2026-01-02 00:55:15 +0300

    mise a jour

--- studio/src/app/page.tsx
+++ studio/src/app/page.tsx
@@ -1,14 +1,5 @@
-"use client";
-
 import HeroSection from '@/components/cine-enroll/hero-section';
-import PathfinderSection from '@/components/cine-enroll/pathfinder-section';
-import CheckerboardSection from '@/components/cine-enroll/checkerboard-section';
-import PricingGridSection from '@/components/cine-enroll/pricing-grid-section';
-import AlumniWallSection from '@/components/cine-enroll/alumni-wall-section';
-import FinalCTASection from '@/components/cine-enroll/final-cta-section';
-import CurriculumSection from '@/components/cine-enroll/curriculum-section';
 import PhotoGallery from '@/components/cine-enroll/photo-gallery';
-import NextStepsSection from '@/components/cine-enroll/next-steps-section';
 import PremiumFooter from '@/components/cine-enroll/premium-footer';
 import { FilmGrainOverlay } from '@/components/ui/cinema-effects';
 
@@ -23,34 +14,12 @@ export default function Home() {
         <HeroSection />
       </div>
 
-      {/* QUEL EST VOTRE BUT ? - Nouveau solid-nav-bar */}
-      <PathfinderSection />
-
-      {/* CHECKERBOARD - Tech d'abord & Une Famille */}
-      <CheckerboardSection />
-
-      {/* TARIFS - Nouveau pricing-grid-section */}
-      <PricingGridSection />
-
-      {/* ALUMNI WALL - Nos Anciens Élèves / Films */}
-      <AlumniWallSection />
-
-      {/* FINAL CTA - Inscriptions 2026 */}
-      <FinalCTASection />
-
-      {/* CURRICULUM - Conservé mais intégré */}
-      <CurriculumSection />
-
-
       {/* PHOTO GALLERY - Conservé (En Immersion sur les Tournages) */}
       <div className="bg-black py-20">
         <div className="container mx-auto px-4">
           <PhotoGallery />
         </div>
       </div>
-
-      {/* PROCHAINE ÉTAPE - Nouveau CTA final */}
-      <NextStepsSection />
 
       {/* FOOTER PREMIUM - Nouveau */}
       <PremiumFooter />
