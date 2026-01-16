34297840715595a95a8e052c8ebf22e22daca9d7
Willston125git config --global user.email <a.willston@gmail.com>
2026-01-16 16:02:16 +0300

    mise a jour

--- studio/src/app/layout.tsx
+++ studio/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import Navbar from '@/components/navbar';
+import WhatsAppButton from '@/components/WhatsAppButton';
 
 export const metadata: Metadata = {
   title: 'Cineworld Académie',
@@ -17,7 +18,9 @@ export default function RootLayout({
       <body>
         <Navbar />
         {children}
+        <WhatsAppButton />
       </body>
     </html>
   );
 }
+
